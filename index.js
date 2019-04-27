5ba61bee76cbfbc31c61e8755324d42c1867aedc
EidenisK <eidenis.gargzdai@gmail.com>
2019-04-27 15:08:59 +0300

    atnaujintas dizainas, pridėtas pasibaigusių pamokų žymėjimas

---
+++ index.js
@@ -0,0 +1,135 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBtl4PbyMarx7DOH2PO-UwdKM0AD3LM80M",
+  authDomain: "kam-dabar-langas.firebaseapp.com",
+  databaseURL: "https://kam-dabar-langas.firebaseio.com",
+  projectId: "kam-dabar-langas",
+  storageBucket: "kam-dabar-langas.appspot.com",
+  messagingSenderId: "777972655000"
+};
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const sarasas = document.getElementById("main-list");
+var displayCarouselAnimation = true;
+
+function vykdyti() {
+  var diena = document.getElementById("diena").value;
+  var pamoka = document.getElementById("pamoka").value -1;
+  var pavadinimas = document.getElementById("pavadinimas").value;
+
+  atnaujintiSarasa(diena, pamoka, pavadinimas);
+}
+
+function atnaujintiSarasa(diena, pamoka, pavadinimas) {
+  console.log(diena + ", " + pamoka + ", " + pavadinimas);
+  sarasas.innerHTML = '<li class="nebera_pamoku"><a><i>Pilka spalva reiškia, kad tą dieną nebėra pamokų</i></a></li>';
+  var path = diena + '/' + pamoka.toString() + '/default'; //visos pamokos tuo metu
+  var docRef = firestore.collection(path);
+
+  firestore.collection(path).get().then(function(snap) {
+    snap.forEach(function(doc) {
+      if(doc.id.includes(pavadinimas)) {
+        var new_path = path + '/' + doc.id + '/default'; //visi vardai
+        firestore.collection(new_path).get().then(function(snap2) {
+          snap2.forEach(function(doc2) {
+            var text = '<li ';
+            if(doc2.data().nebera_pamoku) text += ' class="nebera_pamoku"';
+            text += '><a href="' + doc2.data().nuoroda + '">' + doc2.id + '</a></li>';
+            sarasas.innerHTML += text;
+          });
+        });
+      }
+    });
+  });
+}
+
+var dienos = ['pirmadienis', 'antradienis', 'trečiadienis', 'ketvirtadienis', 'penktadienis'];
+
+var langeliai = document.getElementsByTagName('td');
+Array.from(langeliai).forEach(function(langelis) {
+  var dienos_nr = $(langelis).index(),
+      pamokos_nr = $(langelis).parent().index();
+  langelis.addEventListener("click", function() {
+    if(displayCarouselAnimation) {
+      $("#pamokos-pavadinimas").css("color", "red");
+      $("#pamokos-pavadinimas").focus();
+      displayCarouselAnimation = false;
+    } else {
+      var pavadinimas = $("#pamokos-pavadinimas").text();
+      atnaujintiSarasa(dienos[dienos_nr-1], pamokos_nr-1, pavadinimas);
+    }
+  });
+});
+
+var timeoutvar;
+
+$("#pamokos-pavadinimas").bind("click", function() {
+  if(displayCarouselAnimation) {
+    clearTimeout(timeoutvar);
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: none }";
+    document.body.appendChild(css);
+  }
+  $("#pamokos-pavadinimas").css("color", "white");
+  displayCarouselAnimation = false;
+});
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  if(displayCarouselAnimation)
+    timeoutvar = setTimeout(function() {
+       that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
+  document.body.appendChild(css);
+};
--- script.py
+++ script.py
@@ -1,15 +1,11 @@
 import sys
-
 import requests
 import urllib.request
-#import time
-#import lxml.html as lh
 from bs4 import BeautifulSoup
-#import html5lib
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import datetime
 
 #class Mokinys:
     #laikotarpis = ""
@@ -27,12 +23,12 @@ db = firestore.client()
 
 ###################################
 def nuskaityti_viena(NUORODA):
+    pradzia = datetime.datetime.now()
     dokumentas = str(urllib.request.urlopen(NUORODA).read(), 'windows-1257')
     soup = BeautifulSoup(dokumentas, 'lxml')
 
-    #m = Mokinys()
     PAVADINIMAS = soup.center.text #Gargzdu ,,Vaivorykstes'' gimnazija, 2 pusmetis, nuo sausio 28
-    pamokos = [["" for j in range(6)] for i in range(9)] #tuscias sarasas
+    pamokos = [["" for j in range(6)] for i in range(10)] #tuscias sarasas
 
     lentele = soup.find('table')
     eilutes = lentele.find_all('tr')
@@ -46,19 +42,41 @@ def nuskaityti_viena(NUORODA):
             text = langeliai[langelis].text.strip()
             if text == '':
                 text = 'langas'
+            pamokos[eilute][langelis] = text;
 
-            tempPath = path = sav_dienos[langelis-1] + '/' + str(eilute-2) + '/default/' + text
+    pirma_pabaiga = datetime.datetime.now()
+    print("Scraping done in " + str(pirma_pabaiga - pradzia));
+
+    total = 5 * ( len(eilutes) -2 )
+    current = 0
+
+    for diena in range(1, 6):
+        nebera_pamoku = True;
+
+        for pamoka in range(len(eilutes)-1, 1, -1):
+            if pamokos[pamoka][diena] != 'langas':
+                nebera_pamoku = False;
+
+            tempPath = path = sav_dienos[diena-1] + '/' + str(pamoka-2) + '/default/' + pamokos[pamoka][diena]
             temp_doc = db.document(tempPath)
             temp_doc.set({
                 u'null': u'null'
             })
 
-            path = sav_dienos[langelis-1] + '/' + str(eilute-2) + '/default/' + text + '/default/' + VARDAS;
+            path = sav_dienos[diena-1] + '/' + str(pamoka-2) + '/default/' + pamokos[pamoka][diena] + '/default/' + VARDAS;
             doc_ref = db.document(path)
             doc_ref.set({
                 u'mokytojas': u'null',
-                u'nuoroda': NUORODA
+                u'nuoroda': NUORODA,
+                u'nebera_pamoku': nebera_pamoku
             })
+
+            current += 1
+            print(str(current) + "/" + str(total))
+
+    antra_pabaiga = datetime.datetime.now()
+    print("Upload complete in additional " + str(antra_pabaiga - pirma_pabaiga))
+    print("Total time: " + str(antra_pabaiga - pradzia))
 ###########################################
 
 ##################MAIN######################
